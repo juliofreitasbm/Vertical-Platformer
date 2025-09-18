a63bbd053a9a63fed5302a8123f16428c05b6b4e
Julio <juliofreitas.bm@gmail.com>
2025-09-18 17:42:43 -0300

    Refatoracao das classes

--- index.js
+++ index.js
@@ -16,53 +16,23 @@ for(let i = 0; i < floorCollisions.length; i += 36) {
 console.log(floorCollisions2D);
 
 
+const collisionBlocks = []
+floorCollisions2D.forEach((row) => {
+	row.forEach((symbol) => {
+		if(symbol === 192) {
+			console.log('draw a block');
+			collisionBlocks.push(
+				new CollisionBlock({position: {
+					x: 0,
+					y: 0,
+				}})
+			)
+		}
+	})
+})
+
 const gravity = 0.5;
 
-class Sprite {
-	constructor({position, imageSrc}) {
-		this.position = position
-		this.image = new Image()
-		this.image.src = imageSrc
-	}
-
-	draw() {
-		if(!this.image) return
-		c.drawImage(this.image, this.position.x, this.position.y)
-	}
-
-	update() {
-		this.draw()
-	}
-}
-
-class Player {
-	constructor(position) {
-		this.position = position;
-		this.velocity = {
-			x: 0,
-			y: 1,
-		}
-		this.height = 100;
-	}
-
-	draw() {
-		c.fillStyle = 'red';
-		c.fillRect(this.position.x, this.position.y, 100, this.height)
-	}
-
-	update() {
-		this.draw()
-		this.position.y += this.velocity.y
-		this.position.x += this.velocity.x
-		if(this.position.y + this.height + this.velocity.y< canvas.height) {
-			this.velocity.y += gravity;
-		}
-		else {
-			this.velocity.y = 0
-		}
-			
-	}
-}
 
 const player = new Player({x: 0, y:0})
 const player2 = new Player({x: 300, y:300})
@@ -102,6 +72,8 @@ function animate() {
 	player.velocity.x = 0;
 	if (keys.d.pressed) player.velocity.x = 5
 	else if(keys.a.pressed) player.velocity.x = -5
+
+	collisionBlocks.forEach((block) => {block.update()})
 }
 
 
